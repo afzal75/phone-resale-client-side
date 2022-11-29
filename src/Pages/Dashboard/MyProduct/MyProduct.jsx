dea5d8edc8dd701041c5235821d963615c21ec1b
afzal75 <afzalbhuiyan16@gmail.com>
2022-11-29 23:35:41 +0600

    product deleted done

--- src/Pages/Dashboard/MyProduct/MyProduct.jsx
+++ src/Pages/Dashboard/MyProduct/MyProduct.jsx
@@ -1,23 +1,40 @@
 import { useQuery } from '@tanstack/react-query';
-import { data } from 'autoprefixer';
 import React from 'react';
+import toast from 'react-hot-toast';
 import MyProducts from './MyProducts';
 
 const MyProduct = () => {
-    const { data: products = [], isLoading } = useQuery({
+    const { data: products = [], isLoading, refetch } = useQuery({
         queryKey: ['product'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/products');
             const data = await res.json();
             return data;
         }
-    })
+    });
+
+    const handleDeleteProduct = (product) => {
+        fetch(`http://localhost:5000/products/${product._id}`, {
+            method: 'DELETE',
+            // headers: {
+            //     authorization: `bearer ${localStorage.getItem('accessToken')}`
+            // }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success('Product Deleted Successfully');
+                }
+            })
+    }
     return (
-        <div className='grid gap-6 mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid gap-6 mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
             {
                 products?.map(product => <MyProducts
                     key={product._id}
                     product={product}
+                    handleDeleteProduct={handleDeleteProduct}
                 ></MyProducts>)
             }
         </div>
--- src/Pages/Dashboard/MyProduct/MyProducts.jsx
+++ src/Pages/Dashboard/MyProduct/MyProducts.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-const MyProducts = ({ product }) => {
+const MyProducts = ({ product, handleDeleteProduct }) => {
     const { title, img } = product;
+
+
+    
     return (
         <div className="card shadow-xl">
             <figure className="px-10 pt-10">
-                <img src={img} alt="" className="rounded-xl" />
+                <img src={img} alt="" className="rounded-xl w-2/3" />
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{title}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="flex">
+                    <div>
+                        <button className="btn btn-primary mr-4">Buy Now</button>
+                    </div>
+                    <div>
+                        <button onClick={() => handleDeleteProduct(product)} className="btn btn-primary">Delete</button>
+                    </div>
                 </div>
             </div>
         </div>
